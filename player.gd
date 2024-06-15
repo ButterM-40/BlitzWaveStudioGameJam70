3f784596673a126932a36a22dfbf65247607730f
Butter Man40 <ramiro.santosf40@gmail.com>
2024-06-15 18:00:26 -0500

    Player Animation & UI settings

--- player.gd
+++ player.gd
@@ -1,14 +1,20 @@
 extends CharacterBody2D
 
-
+@export var player_sprite: AnimatedSprite2D
+@onready var initial_sprite_scale = player_sprite.scale
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var state = PlayerState.IDLE
+enum PlayerState{
+	IDLE,
+	WALK
+}
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var horizontal_input = (
 		
 		Input.get_action_strength("Left")
@@ -17,3 +23,24 @@ func _physics_process(delta):
 	 #Input.get_vector("Left", "Right", 0, 0)
 	velocity.x = horizontal_input * SPEED
 	move_and_slide()
+	handle_movement_state()
+	face_movement_direction(horizontal_input)
+	
+func face_movement_direction(horizontal_input):
+	if not is_zero_approx(horizontal_input): 
+		if horizontal_input < 0:
+			player_sprite.scale = Vector2(-initial_sprite_scale.x, initial_sprite_scale.y)
+		else:
+			player_sprite.scale = initial_sprite_scale
+func handle_movement_state():
+	if is_zero_approx(velocity.x):
+		state = PlayerState.IDLE
+	
+	elif not is_zero_approx(velocity.x):
+		state = PlayerState.WALK
+	match state:
+		PlayerState.IDLE:
+			player_sprite.play("idle")
+		PlayerState.WALK:
+			player_sprite.play("walk")
+		
