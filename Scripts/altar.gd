3bdef0408f2ef9ddddb5f7dd73d2759675365f09
WellHiIGuess <67477071+WellHiIGuess@users.noreply.github.com>
2024-06-23 20:50:37 +0200

    the end is never the end....

--- Scripts/altar.gd
+++ Scripts/altar.gd
@@ -4,5 +4,6 @@ class_name Altar
 @export var required_totem: Constants.TotemType
 
 func _on_body_entered(body: Node2D) -> void:
+	print('entered:', body)
 	if body.type == required_totem:
 		activate()
