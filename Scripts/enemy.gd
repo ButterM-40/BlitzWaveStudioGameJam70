c63e3b40b58446997283fdb1c1864f767c2a573e
WellHiIGuess <67477071+WellHiIGuess@users.noreply.github.com>
2024-06-23 12:02:51 +0200

    Made enemy work

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,6 +1,21 @@
 extends RigidBody2D
 class_name Enemy
 
+@export var speed: float
+@export var starting_direction: StartingDirection = StartingDirection.LEFT
+var direction: Vector2 = Vector2.LEFT
+
+enum StartingDirection {
+	LEFT,
+	RIGHT
+}
+
+func _physics_process(delta: float) -> void:
+	var motion: Vector2 = direction * speed * delta
+	var kb: KinematicCollision2D = move_and_collide(motion)
+	if kb && kb.get_normal() != Vector2.UP:
+		direction = -1 * direction
+
 func _on_died() -> void:
 	# Death logic
 	call_deferred("queue_free")
