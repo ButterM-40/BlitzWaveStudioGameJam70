7acada581d3287287413c3f572dfe5b6b6d2f9cd
Laozey <Qepron@hotmail.fr>
2024-06-23 21:03:47 +0200

    Export and level 11

--- Scripts/Components/hurt_box.gd
+++ Scripts/Components/hurt_box.gd
@@ -1,7 +1,10 @@
 extends Area2D
 class_name HurtBox
 
+@export var hurt_sounds: Array[AudioStreamWAV]
+
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -11,3 +14,6 @@ func _on_body_entered(body: Node2D) -> void:
 		if child is Health:
 			child.take_damage()
 			break
+	
+	audio_stream_player_2d.stream = hurt_sounds.pick_random()
+	audio_stream_player_2d.play()
