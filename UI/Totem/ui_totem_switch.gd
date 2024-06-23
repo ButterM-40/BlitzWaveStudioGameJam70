9276ea8f1937cf584f14d5546dd7923d9761d881
WellHiIGuess <67477071+WellHiIGuess@users.noreply.github.com>
2024-06-23 08:15:50 +0200

    Added mini images

--- UI/Totem/ui_totem_switch.gd
+++ UI/Totem/ui_totem_switch.gd
@@ -88,6 +88,10 @@ func display_current_totem(c):
 	pass
 func display_stored_totem(sprite, current_i, max_size):
 	sprite.get_child(0).visible = false
+
+	var old_sprite = sprite.get_node('Icon').texture.resource_path
+	print(old_sprite)
+	sprite.get_node('Icon').texture = load('res://Art/Icons/mini' + old_sprite.substr(21, 10))
 	#Child 0 is Selected
 	#Child 1 is TotemColor
 	var totemDisplayType = sprite.get_child(1)
