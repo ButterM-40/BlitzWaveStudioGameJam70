023da34d40c2a472ddb5362d543908f121fadaab
WellHiIGuess <67477071+WellHiIGuess@users.noreply.github.com>
2024-06-23 17:22:56 +0200

    Made R Button also reset

--- UI/ResetButton/ResetButton.gd
+++ UI/ResetButton/ResetButton.gd
@@ -1,5 +1,11 @@
 extends Button
 
+func _process(delta):
+	if Input.is_action_just_pressed('Reset'):
+		$music.play()
+		get_tree().reload_current_scene()
+	pass
+
 func _on_pressed():
 	$music.play()
 	get_tree().reload_current_scene()
