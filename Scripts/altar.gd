b86fd883889a72bfb9f9d03efa3e833a33381af2
Laozey <Qepron@hotmail.fr>
2024-06-24 00:01:26 +0200

    Fix

--- Scripts/altar.gd
+++ Scripts/altar.gd
@@ -3,6 +3,11 @@ class_name Altar
 
 @export var required_totem: Constants.TotemType
 
+@onready var light_sprite: AnimatedSprite2D = $LightSprite
+
+func _ready() -> void:
+	light_sprite.frame = required_totem
+
 func _on_body_entered(body: Node2D) -> void:
 	print('entered:', body)
 	if body.type == required_totem:
