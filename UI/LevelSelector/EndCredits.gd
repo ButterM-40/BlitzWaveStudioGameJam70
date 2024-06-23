2619dec28312a403dbadcebc76e7f41a59cad3c1
WellHiIGuess <67477071+WellHiIGuess@users.noreply.github.com>
2024-06-23 19:39:39 +0200

    It work now

--- UI/LevelSelector/EndCredits.gd
+++ UI/LevelSelector/EndCredits.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const LEVEL_BTN = preload("res://UI/LevelSelector/LvlBtn.tscn")
 
+func _ready():
+	Music.music_playing = false
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file('res://Scenes/MainMenu.tscn')
